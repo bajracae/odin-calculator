fc6f3562de251a6685a5c9fb91de79b58e793f1c
bajracae <aeijanbajracharya@gmail.com>
2025-05-13 22:38:15 -0700

    added add, subtract, multiply, divide, and operator functions

---
+++ calculator.js
@@ -0,0 +1,44 @@
+const add = function (a, b) {
+	return a + b;
+};
+
+const subtract = function (a, b) {
+	return a - b;
+};
+
+const multiply = function (a, b) {
+	return a * b;
+};
+
+const divide = function (a, b) {
+	return a / b;
+};
+
+const operate = function (a, operator, b) {
+	switch (operator) {
+		case "+":
+			result = add(a, b);
+			break;
+		case "-":
+			result = subtract(a, b);
+			break;
+		case "*":
+			result = multiply(a, b);
+			break;
+		case "/":
+			result = divide(a, b);
+			break;
+		default:
+			result = NaN; // Questioning if this is even needed but will come back later
+	}
+	return result;
+};
+
+// Do not edit below this line
+module.exports = {
+	add,
+	subtract,
+	multiply,
+	divide,
+	operate,
+};
